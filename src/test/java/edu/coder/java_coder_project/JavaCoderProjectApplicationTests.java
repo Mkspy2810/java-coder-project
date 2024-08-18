dbedcff05c5869e90963c641a78f2684f1e54c2b
Thayrov Garcia <105318081+Thayrov@users.noreply.github.com>
2024-08-17 23:42:02 -0600

    springboot init

---
+++ src/test/java/edu/coder/java_coder_project/JavaCoderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.coder.java_coder_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
